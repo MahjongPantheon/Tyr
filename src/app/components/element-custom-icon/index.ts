eb0b156290f893e15b01d98ab22de21310a79d8c
Oleg Klimenko <o.klimenko@2gis.ru>
2016-12-31 19:28:17 +0700

    Move all graphics to svg, inline all svg to avoid loading lags

--- src/app/components/element-custom-icon/index.ts
+++ src/app/components/element-custom-icon/index.ts
@@ -1,18 +1,24 @@
-import { DomSanitizer } from '@angular/platform-browser';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { Component, Input } from '@angular/core';
+import { icons } from './icons';
 
 @Component({
   selector: 'custom-icon',
-  template: `
-    <img 
-      src="assets/{{type}}.svg" 
-      [style.width]="resize + 'px'" 
-      [style.height]="resize + 'px'"
-     />
-  `
+  template: `<i style='display: inline-block'
+        [innerHTML]="content"
+        [style.width]="resize + 'px'" 
+        [style.height]="resize + 'px'"
+      ></i>`
 })
 export class CustomIconComponent {
   @Input() type: string;
   @Input() resize: number = 28;
+  content: SafeHtml = '';
+
+  constructor(private sanitizer: DomSanitizer) { }
+
+  ngOnInit() {
+    this.content = this.sanitizer.bypassSecurityTrustHtml(icons[this.type]);
+  }
 }
 
