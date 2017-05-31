9b97aab155d6f093eb9daa2d62293a98fcc2d629
Oleg Klimenko <o.klimenko@2gis.ru>
2017-05-31 22:10:06 +0300

    TYR-33 kick players to overview when game is started

--- src/app/primitives/appstate/index.ts
+++ src/app/primitives/appstate/index.ts
@@ -150,6 +150,12 @@ export class AppState {
         if (gameConfig.useTimer) {
           initTimer(timerState.timeRemaining);
         }
+
+        // Player is now in game, so kick him to overview from watching
+        if (this._currentScreen === 'otherTable' || this._currentScreen === 'otherTablesList') {
+          this._currentScreen = 'overview';
+        }
+
         this.updateOverview();
       } else {
         // no games! Or game ended just now
@@ -253,6 +259,7 @@ export class AppState {
     switch (this._currentScreen) {
       case 'overview':
         this._currentScreen = 'otherTablesList';
+        this.updateCurrentGames(); // update games list to prevent players in game from watching games
         break;
       default: ;
     }
